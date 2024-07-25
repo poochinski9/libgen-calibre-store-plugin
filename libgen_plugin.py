a821128bc91325315ba55bd7fd02f783c82e2bbb
poochinski <89724812+poochinski9@users.noreply.github.com>
2024-07-25 14:29:01 -0500

    Major update to use libgen.gs to support fiction, scihub, and more (#15)

--- libgen_plugin.py
+++ libgen_plugin.py
@@ -1,55 +1,149 @@
 # -*- coding: utf-8 -*-
-# License: GPLv3 Copyright: 2023, poochinski9
+# License: GPLv3 Copyright: 2024, poochinski9
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import urllib.parse
+import logging
+import re
+import sys
 import time
-
-from bs4 import BeautifulSoup
+import urllib.parse
 
 from PyQt5.Qt import QUrl
-
+from bs4 import BeautifulSoup
 from calibre import browser, url_slash_cleaner
 from calibre.gui2 import open_url
 from calibre.gui2.store import StorePlugin
-from calibre.gui2.store.search_result import SearchResult
 from calibre.gui2.store.basic_config import BasicStoreConfig
+from calibre.gui2.store.search_result import SearchResult
 from calibre.gui2.store.web_store_dialog import WebStoreDialog
 
-BASE_URL = "https://libgen.is"
+BASE_URL = "https://libgen.gs"
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"
+
+# Declare global variables at the module level
+title_index = None
+image_index = None
+author_index = None
+year_index = None
+pages_index = None
+size_index = None
+ext_index = None
+mirrors_index = None
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 #####################################################################
 # Plug-in base class
 #####################################################################
 def search_libgen(query, max_results=10, timeout=60):
     res = "25" if max_results <= 25 else "50" if max_results <= 50 else "100"
-    search_url = f"{BASE_URL}/search.php?req={query.decode()}&res={res}&view=simple"
-    print("searching: " + search_url)
-    print("max results: " + str(max_results))
+    search_url = f"{BASE_URL}/index.php?req={query.decode('utf-8')}&columns[]=t&columns[]=a&columns[]=s&columns[]=y&columns[]=p&columns[]=i&objects[]=f&objects[]=e&objects[]=s&objects[]=a&objects[]=p&objects[]=w&topics[]=l&topics[]=c&topics[]=f&topics[]=a&topics[]=m&topics[]=r&topics[]=s&res={res}&filesuns=all&covers=on"
+    logger.info("searching: " + search_url)
+    logger.info("max results: " + str(max_results))
     br = browser(user_agent=USER_AGENT)
     raw = br.open(search_url).read()
     soup = BeautifulSoup(raw, "html5lib")
 
-    # Find the rows that represent each book, and skip the first item which is table headers
-    trs = soup.select('table[class="c"] > tbody > tr')[1:]
+    extract_indices(soup)
 
-    #map the trs to search results, filter out items that dont have a title or author, and limit it to max_results size
+    # Find the rows that represent each book, and skip the first item which is table headers
+    trs = soup.select('table[class="table table-striped"] > tbody > tr')
+
+    # map the trs to search results, filter out items that dont have a title or author, and limit it to max_results size
     return [result for result in map(build_search_result, trs) if result.title and result.author][:max_results]
 
+
+def extract_indices(soup):
+    elements = ['Author(s)', 'Year', 'Pages', 'Size', 'Ext', 'Mirrors']
+    indices = {}
+
+    for idx, th in enumerate(soup.find_all('th')):
+        for element in elements:
+            if element in th.get_text():
+                indices[element] = idx
+
+    global author_index, year_index, pages_index, size_index, ext_index, mirrors_index, title_index, image_index
+
+    image_index = 0
+    title_index = 1
+    author_index = indices.get('Author(s)')
+    year_index = indices.get('Year')
+    pages_index = indices.get('Pages')
+    size_index = indices.get('Size')
+    ext_index = indices.get('Ext')
+    mirrors_index = indices.get('Mirrors')
+
+
+def transform_download_url(url):
+    # Pattern for the first format: /ads23875abc (where abc can be letters and numbers)
+    pattern1 = re.compile(r'/ads([a-fA-F0-9]+)')
+    # Pattern for the second format: /ads.php?md5=235798237abc (where abc can be letters and numbers)
+    pattern2 = re.compile(r'/ads\.php\?md5=([a-fA-F0-9]+)')
+
+    # Check and transform the first format
+    match1 = pattern1.match(url)
+    if match1:
+        return f'/get.php?md5={match1.group(1)}'
+
+    # Check and transform the second format
+    match2 = pattern2.match(url)
+    if match2:
+        return f'/get.php?md5={match2.group(1)}'
+
+    # If no match, return the original URL
+    return url
+
+
 def build_search_result(tr):
-    tds = tr.select('td')
+    tds = tr.find_all('td')
     s = SearchResult()
-    s.title = tds[2].select_one("a:last-of-type").text
-    s.author = tds[1].text
-    s.detail_item = BASE_URL + "/" + tds[2].select_one("a:last-of-type").get("href")
-    size = tds[7].text
-    pages = tds[5].text
-    year = tds[4].text
-    s.price = f"{size}\n{pages} pages\n{year}"  # use price column to display more size, pages, year
-    s.formats = tds[8].text.upper()
+
+    # Extracting the title
+    title_links = tds[title_index].find_all("a", href=True)
+    s.title = " ".join(link.text.strip() for link in title_links if link.text.strip())
+
+    # Extracting the author
+    s.author = tds[author_index].text.strip()
+
+    # Extracting size, pages, and year
+    size = tds[size_index].text.strip()
+
+    pages = tds[pages_index].text.strip()
+
+    year = tds[year_index].text.strip()
+
+    if pages == "0 pages":
+        s.price = f"{size}\n{year}"
+    else:
+        s.price = f"{size}\n{pages} pages\n{year}"
+
+    # Extracting formats
+    s.formats = tds[ext_index].text.strip().upper()
+
+    # Download url
+
+    # Extracting the details URL
+    first_link_in_last_td = tds[mirrors_index].find("a", href=True)
+
+    # Details url:
+    s.detail_item = BASE_URL + first_link_in_last_td["href"].replace("get.php", "ads.php")
+
+    # Setting DRM status
     s.drm = SearchResult.DRM_UNLOCKED
-    s.mirror1_url = tds[9].select_one("a:first-of-type").get("href")
+
+    # Extracting image
+    try:
+        logger.info(tds[image_index])
+        image_src = tds[image_index].find("img").get("src")
+    except:
+        image_src = None
+
+    if image_src:
+        s.cover_url = BASE_URL + image_src
+
     return s
 
 
@@ -65,31 +159,39 @@ class LibgenStorePlugin(BasicStoreConfig, StorePlugin):
             d.set_tags(self.config.get("tags", ""))
             d.exec_()
 
-    def search(self, query, max_results=10, timeout=60):
+    @staticmethod
+    def get_details(search_result, retries=3):
+        s = search_result
+        br = browser(user_agent=USER_AGENT)
+        for attempt in range(retries):
+            try:
+                raw = br.open(s.detail_item).read()
+                break
+            except:
+                logger.info("server error, retrying load of " + s.detail_item)
+                # server error, retry after delay
+                time.sleep(1)
+
+        soup2 = BeautifulSoup(raw, "html5lib")
+        # Select the first <a> tag inside the div with id="download"
+        download_a = soup2.select_one('tr a')
+        if download_a:
+            download_url = download_a.get("href")
+        else:
+            # Handle the case where the download link is not found
+            download_url = None
+
+        new_base_url = urllib.parse.urlparse(s.detail_item).hostname
+
+        s.downloads[s.formats] = "https://" + new_base_url + "/" + download_url
+
+    @staticmethod
+    def search(query, max_results=10, timeout=60):
         for result in search_libgen(query, max_results=max_results, timeout=timeout):
             yield result
 
-    def get_details(self, search_result, timeout):
-        s = search_result
-        br = browser(user_agent=USER_AGENT)
-        while True:
-            try:
-                raw = br.open(s.mirror1_url).read()
-                break
-            except:
-                # sever error, retry after delay
-                time.sleep(0.1)
-
-        soup2 = BeautifulSoup(raw, "html5lib")
-        download_a = soup2.select('div[id="download"] > ul > li > a')[0]
-        download_url = download_a.get("href")
-
-        new_base_url = urllib.parse.urlparse(s.mirror1_url).hostname
-        image_url = "http://" + new_base_url + soup2.select("img")[0].get("src")
-
-        s.downloads[s.formats] = download_url
-        s.cover_url = image_url
 
 if __name__ == "__main__":
+    query_string = " ".join(sys.argv[1:])
     for result in search_libgen(bytes(" ".join(sys.argv[1:]), "utf-8")):
         print(result)
